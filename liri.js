d7b8284174ce66bdbb204268106d9eda1d5c689a
Katie Vogel <kvog983@gmail.com>
2019-06-30 17:11:52 -0400

    omdb api functioning

--- liri.js
+++ liri.js
@@ -17,24 +17,34 @@ spotify.search({ type: 'track', query: process.argv[3] }, function(err, data) {
     
   })
 };
+var movieName = process.argv[3];
 
-// axios
-//   .get("http://www.omdbapi.com/?i=tt3896198&apikey=3eb0741a")
-//   .then(function(response) {
-//     console.log(response.data);
-//   })
-//   .catch(function(error) {
-//     if (error.response) {
-//       console.log(error.response.data);
-//       console.log(error.response.status);
-//       console.log(error.response.headers);
-//     } else if (error.request) {
-//       console.log(error.request);
-//     } else {
-//       console.log("Error", error.message);
-//     }
-//     console.log(error.config);
-//   });
+if (process.argv[2] === "movie-this") {
+axios
+  .get("http://www.omdbapi.com/?t=" + movieName + "&apikey=3eb0741a")
+  .then(function(response) {
+    console.log("Movie Title: " + response.data.Title);
+    console.log("Release Year: " + response.data.Year);
+    console.log("IMDB Rating: " + response.data.imdbRating);
+    console.log("Rotten Tomatoes: " + response.data.Ratings[1].Value);
+    console.log("Production Country: " + response.data.Country);
+    console.log("Language(s): " + response.data.Language);
+    console.log("Plot Summary: " + response.data.Plot);
+    console.log("Leading Cast: " + response.data.Actors);
+  })
+  .catch(function(error) {
+    if (error.response) {
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      console.log(error.request);
+    } else {
+      console.log("Error", error.message);
+    }
+    console.log(error.config);
+  })
+};
 
 //   axios
 //   .get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")

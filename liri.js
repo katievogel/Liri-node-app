06bfd68636662784ca8abff7ce6f97511ea5a4a7
Katie Vogel <kvog983@gmail.com>
2019-06-30 17:53:21 -0400

    concert api functioning with moment

--- liri.js
+++ liri.js
@@ -3,6 +3,7 @@ var Spotify = require("node-spotify-api");
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
 var axios = require("axios");
+var moment = require('moment');
 
 if (process.argv[2] === "spotify-this-song") {
 spotify.search({ type: 'track', query: process.argv[3] }, function(err, data) {
@@ -46,20 +47,29 @@ axios
   })
 };
 
-//   axios
-//   .get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
-//   .then(function(response) {
-//     console.log(response.data);
-//   })
-//   .catch(function(error) {
-//     if (error.response) {
-//       console.log(error.response.data);
-//       console.log(error.response.status);
-//       console.log(error.response.headers);
-//     } else if (error.request) {
-//       console.log(error.request);
-//     } else {
-//       console.log("Error", error.message);
-//     }
-//     console.log(error.config);
-//   });
+var artist= process.argv[3];
+if (process.argv[2] === "concert-this")
+  axios
+  .get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+  .then(function(response) {
+    console.log(response.data.length)
+    for (var i = 0; i < response.data.length; ++i) {
+        console.log(response.data[i].venue.name);
+        console.log(response.data[i].venue.city + ", " + response.data[i].venue.region + ", " + response.data[i].venue.country);
+        var concertDate = moment(response.data[i].datetime).format("MM/DD/YYYY");
+        console.log(concertDate);
+    }
+    
+  })
+  .catch(function(error) {
+    if (error.response) {
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      console.log(error.request);
+    } else {
+      console.log("Error", error.message);
+    }
+    console.log(error.config);
+  });

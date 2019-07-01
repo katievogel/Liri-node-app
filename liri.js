8969b37fb043dd3c061d125bfc945fe8c541077d
Katie Vogel <kvog983@gmail.com>
2019-06-30 20:34:55 -0400

    do-what-it-says functioning. note, quotes bad

--- liri.js
+++ liri.js
@@ -7,89 +7,103 @@ var moment = require('moment');
 var fs = require("fs");
 
 var spotSong = process.argv[3];
-function spotifyThis () {
-if (process.argv[2] === "spotify-this-song") {
-spotify.search({ type: 'track', query: spotSong }, function(err, data) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
-    } 
-    var firstItem = data.tracks.items[0];
-    console.log("Artist: " + firstItem.artists[0].name);
-    console.log("Song Name: " + firstItem.name); 
-    console.log("Listen to the song: " + firstItem.external_urls.spotify);
-    console.log("Album Title: " + firstItem.album.name); 
-  })
-}};
-spotifyThis();
-
-var movieName = process.argv[3];
-if (process.argv[2] === "movie-this") {
-axios
-  .get("http://www.omdbapi.com/?t=" + movieName + "&apikey=3eb0741a")
-  .then(function(response) {
-    console.log("Movie Title: " + response.data.Title);
-    console.log("Release Year: " + response.data.Year);
-    console.log("IMDB Rating: " + response.data.imdbRating);
-    console.log("Rotten Tomatoes: " + response.data.Ratings[1].Value);
-    console.log("Production Country: " + response.data.Country);
-    console.log("Language(s): " + response.data.Language);
-    console.log("Plot Summary: " + response.data.Plot);
-    console.log("Leading Cast: " + response.data.Actors);
-  })
-  .catch(function(error) {
-    if (error.response) {
-      console.log(error.response.data);
-      console.log(error.response.status);
-      console.log(error.response.headers);
-    } else if (error.request) {
-      console.log(error.request);
-    } else {
-      console.log("Error", error.message);
-    }
-    console.log(error.config);
-  })
-};
-
-var artist= process.argv[3];
-if (process.argv[2] === "concert-this")
-  axios
-  .get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
-  .then(function(response) {
-    console.log(response.data.length)
-    for (var i = 0; i < response.data.length; ++i) {
-        console.log(response.data[i].venue.name);
-        console.log(response.data[i].venue.city + ", " + response.data[i].venue.region + ", " + response.data[i].venue.country);
-        var concertDate = moment(response.data[i].datetime).format("MM/DD/YYYY");
-        console.log(concertDate);
-    }
-  })
-  .catch(function(error) {
-    if (error.response) {
-      console.log(error.response.data);
-      console.log(error.response.status);
-      console.log(error.response.headers);
-    } else if (error.request) {
-      console.log(error.request);
-    } else {
-      console.log("Error", error.message);
-    }
-    console.log(error.config);
-  });
-
-
-  if (process.argv[2] === "do-what-it-says") {
-  fs.readFile("random.txt", "utf8", function(err, data) {
-    if (err) {
-      return console.log(err);
-    }
-    var output = data.split(",");
-    for (var i = 0; i < output.length; i++) {
-        if (output[0] === "spotify-this-song") {
-            spotSong = output[1];
-            spotifyThis();
-            console.log(spotSong);
+function spotifyThis(spotSong) {
+    spotify.search({ type: 'track', query: spotSong }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
         }
-      ;
-    }
-  })
+        var firstItem = data.tracks.items[0];
+        console.log("Artist: " + firstItem.artists[0].name);
+        console.log("Song Name: " + firstItem.name);
+        console.log("Listen to the song: " + firstItem.external_urls.spotify);
+        console.log("Album Title: " + firstItem.album.name);
+    })
 };
+
+
+
+
+function dispatch(command, argument) {
+    if (command === "spotify-this-song") {
+        spotifyThis(argument)
+    } else if (command === "concert-this") {
+        concertThis(argument)
+    } else if (command === "movie-this") {
+        movieThis(argument)
+    } else if (command === "do-what-it-says") {
+        doWhatItSays(argument)
+    }
+}
+
+var command = process.argv[2];
+var argument = process.argv[3];
+dispatch(command, argument);
+
+
+function movieThis(movieName) {
+    axios
+        .get("http://www.omdbapi.com/?t=" + movieName + "&apikey=3eb0741a")
+        .then(function (response) {
+            console.log("Movie Title: " + response.data.Title);
+            console.log("Release Year: " + response.data.Year);
+            console.log("IMDB Rating: " + response.data.imdbRating);
+            console.log("Rotten Tomatoes: " + response.data.Ratings[1].Value);
+            console.log("Production Country: " + response.data.Country);
+            console.log("Language(s): " + response.data.Language);
+            console.log("Plot Summary: " + response.data.Plot);
+            console.log("Leading Cast: " + response.data.Actors);
+        })
+        .catch(function (error) {
+            if (error.response) {
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            } else if (error.request) {
+                console.log(error.request);
+            } else {
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        })
+};
+
+function concertThis(artist) {
+    var url = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+    axios
+        .get(url)
+        .then(function (response) {
+            console.log(url);
+            console.log(response.data.length)
+            for (var i = 0; i < response.data.length; ++i) {
+                console.log(response.data[i].venue.name);
+                console.log(response.data[i].venue.city + ", " + response.data[i].venue.region + ", " + response.data[i].venue.country);
+                var concertDate = moment(response.data[i].datetime).format("MM/DD/YYYY");
+                console.log(concertDate);
+            }
+        })
+        .catch(function (error) {
+            if (error.response) {
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            } else if (error.request) {
+                console.log(error.request);
+            } else {
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
+}
+
+function doWhatItSays(argument) {
+    fs.readFile("random.txt", "utf8", function (err, data) {
+        if (err) {
+            return console.log(err);
+        }
+        var output = data.split(",");
+        var command = output[0];
+        var argument = output[1];
+        dispatch(command, argument);
+    })
+}
+;
